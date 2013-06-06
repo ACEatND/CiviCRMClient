329d300d2fd301b24c5936034c456cea15447f87
Brad Stalcup <bstalcup@nd.edu>
2013-06-06 11:55:01 -0300

    Update apply.sh
    commented it out so you can't accidentally lose it

--- apply.sh
+++ apply.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-for i in  /mnt/data/vhosts/ace.nd.edu/httpdocs/civiCRMClient//*
-	do
-	if [ -d "$i" ]; then
-		echo "ignoring directory $i"
-	else
-		echo "/***************************************************************
-**
-**        Developed by the Alliance for Catholic Education
-**                at the University of Notre Dame
-**
-****************************************************************/" | cat - $i > /tmp/bradtmp
-		cat /tmp/bradtmp > $i
-		rm /tmp/bradtmp
-	fi
-done
+#for i in  /mnt/data/vhosts/ace.nd.edu/httpdocs/civiCRMClient//*
+#	do
+#	if [ -d "$i" ]; then
+#		echo "ignoring directory $i"
+#	else
+#		echo "/***************************************************************
+#**
+#**        Developed by the Alliance for Catholic Education
+#**                at the University of Notre Dame
+#**
+#****************************************************************/" | cat - $i > /tmp/bradtmp
+#		cat /tmp/bradtmp > $i
+#		rm /tmp/bradtmp
+#	fi
+#done
